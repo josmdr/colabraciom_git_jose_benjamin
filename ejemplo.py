da7621d00fbc7ba526312fd4dd7e27bc12e466ee
josmdr <josm.donoso@duocuc.com>
2025-06-25 13:35:12 -0400

    importefuncion1

--- ejemplo.py
+++ ejemplo.py
@@ -1,1 +1,6 @@
 print("hola")
+import funcion_1
+ejem=int(input("ingrese un valor"))
+ejem1=int(input("ingrese un valor"))
+resultado=funcion_1.suma(ejem,ejem1)
+print(resultado)
--- funcion_1.py
+++ funcion_1.py
@@ -1,7 +1,2 @@
 def suma (a,b):
     return a+b
-
-ejem=int(input("ingrese un valor"))
-ejem1=int(input("ingrese un valor"))
-resultado=suma(ejem,ejem1)
-print(resultado)
